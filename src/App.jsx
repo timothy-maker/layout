1245267d8266366789fc5da9cfe37f096d5fab27
timothy-maker <ltpaquino.student@ua.edu.ph>
2025-09-04 11:47:52 +0800

    updated footer

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
          <div className="rp">Related Posts</div>
       </div>
     </div>
-    <div className="footer">BSIT-3B</div>
+    <div className="footer">C-PCIT9 BSIT-3B</div>
   </div>
  )
 }
